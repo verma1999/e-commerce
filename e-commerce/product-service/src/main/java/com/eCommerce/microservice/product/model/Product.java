d2b736b9221967509b91e032dfbd98a6f661b914
Aditya Verma <verma.aditya1999@gmail.com>
2024-11-04 00:26:27 +0530

    product service

---
+++ e-commerce/product-service/src/main/java/com/eCommerce/microservice/product/model/Product.java
@@ -0,0 +1,21 @@
+package com.eCommerce.microservice.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
---
+++ e-commerce/product-service/src/test/java/com/eCommerce/microservice/product/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.eCommerce.microservice.product;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
