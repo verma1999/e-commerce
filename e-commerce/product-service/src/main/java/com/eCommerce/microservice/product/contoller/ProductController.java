ce8bc6a04869e8d0557e86d1abcd8dd672d53604
Aditya Verma <verma.aditya1999@gmail.com>
2024-12-09 01:18:59 +0530

    swagger and circuit breaker impl

--- e-commerce/product-service/src/main/java/com/eCommerce/microservice/product/contoller/ProductController.java
+++ e-commerce/product-service/src/main/java/com/eCommerce/microservice/product/contoller/ProductController.java
@@ -34,6 +34,7 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
+
         return productService.getAllProducts();
     }
 
